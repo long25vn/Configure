3c8d17f62551316ca1a58afa90150025e2d62bbc
Seventy Nine <long25vn@gmail.com>
2018-09-02 22:28:11 +0700

    update

--- setGoPath.sh
+++ setGoPath.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
-echo 'export GOPATH=$HOME/go' >>~/.profile
+echo 'export GOPATH=$HOME/go' >>~/.profile 
 echo 'export PATH=$PATH:$GOPATH/bin' >>~/.profile
+source ~/.profile

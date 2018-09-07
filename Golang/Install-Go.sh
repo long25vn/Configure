37d87d9c80764a3962a645e4a21dc15c1e398ad7
Seventy Nine <31624384+long25vn@users.noreply.github.com>
2018-09-07 11:07:19 +0700

    Update Install-Go.sh

--- Golang/Install-Go.sh
+++ Golang/Install-Go.sh
@@ -10,5 +10,10 @@ tar -xvf $GOVERSION.linux-amd64.tar.gz
 rm -rf /usr/local/go
 mv go /usr/local
 rm $GOVERSION.linux-amd64.tar.gz
-touch ~/.config/fish/config.fish
+
+
 echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
+echo 'export GOPATH=$HOME/go' >>~/.profile 
+echo 'export PATH=$PATH:$GOPATH/bin' >>~/.profile
+
+source ~/.profile

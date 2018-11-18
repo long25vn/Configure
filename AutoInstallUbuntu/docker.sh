bce047ca1d67021b76454a4801024916cdfa28e4
Thanh Long <long25vn@gmail.com>
2018-11-18 20:53:12 +0700

    add install Docker-compose

--- AutoInstallUbuntu/docker.sh
+++ AutoInstallUbuntu/docker.sh
@@ -16,3 +16,11 @@ sudo usermod -a -G docker $USER
 systemctl enable docker
 
 echo 'Done!'
+
+echo 'Install Docker Compose'
+
+curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version

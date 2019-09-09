8bd5794f03c43213782ad4bc2a41b6b27d8138e4
Seventy Nine <31624384+long25vn@users.noreply.github.com>
2019-09-09 11:30:39 +0700

    Update Set-GOPATH.sh

--- Golang/Set-GOPATH.sh
+++ Golang/Set-GOPATH.sh
@@ -3,4 +3,5 @@ echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
 echo 'export GOPATH=$HOME/go' >>~/.profile 
 echo 'export PATH=$PATH:$GOPATH/bin' >>~/.profile
 source ~/.profile
+# Tạo đường dẫn
 go get github.com/satori/go.uuid
